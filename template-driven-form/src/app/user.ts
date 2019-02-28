7a4875ca7a82351ee2d20f3ee959121efd9e87ae
ankurjavia226 <javiaankur007@gmail.com>
2019-02-28 22:34:47 +0530

    angular-forms

--- template-driven-form/src/app/user.ts
+++ template-driven-form/src/app/user.ts
@@ -1,10 +1,8 @@
 export class User {
-    constructor(
-        public name: String,
-        public email: String,
-        public phone: number,
-        public topic: string,
-        public timePreference: string,
+        public name: String;
+        public email: String;
+        public phone: number;
+        public topic: string;
+        public timePreference: string;
         public subscribe: boolean
-    ) {}
 }
