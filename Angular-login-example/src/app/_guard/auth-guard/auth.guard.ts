8b7d0436851ac1e84f56f93f47236f1d5842a23b
ankurjavia226 <javiaankur007@gmail.com>
2019-03-04 17:35:55 +0530

    angular-practice-example

--- Angular-login-example/src/app/_guard/auth-guard/auth.guard.ts
+++ Angular-login-example/src/app/_guard/auth-guard/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from 'src/app/_shared/services';
+import { AppStorage } from 'src/app/_shared/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,15 @@ import { AuthService } from 'src/app/_shared/services';
 export class AuthGuard implements CanActivate {
   sessionStorageKey: string =  'loggedinUser';
 
-  constructor(private _authService: AuthService){}
+  constructor(private _authService: AuthService, private _router: Router){}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
   
+    if(!sessionStorage.getItem(AppStorage.USER_KEY)){
+      this._router.navigate(['/login']);
+    }
     return !!this._authService.isLoggedIn;  
   }
 } 
