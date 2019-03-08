e042a1dc53534a1a8c1ba00fff9f07e96e548973
ankurjavia226 <javiaankur007@gmail.com>
2019-03-08 13:01:28 +0530

    angular-practice-exampel

---
+++ oshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAW3JVCqxh4lHIs44f776LwcGdC_Gjg1zo",
+    authDomain: "oshop-b44d1.firebaseapp.com",
+    databaseURL: "https://oshop-b44d1.firebaseio.com",
+    projectId: "oshop-b44d1",
+    storageBucket: "oshop-b44d1.appspot.com",
+    messagingSenderId: "720654821122"
+  }
+};
